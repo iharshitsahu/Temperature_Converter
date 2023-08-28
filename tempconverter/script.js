36683687d604ae9b55b044d0484056a3f2bfa479
Harshit Sahu <134074035+iharshitsahu@users.noreply.github.com>
2023-08-28 18:28:20 +0530

    Add files via upload

---
+++ tempconverter/script.js
@@ -0,0 +1,26 @@
+const calculateTemp = ()=> {
+    const numberTemp = document.getElementById('temp').value;
+    
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp= temp_diff.options[tempSelected.selectedIndex].value;
+
+    const celToFah=(cel) =>{
+        let fahrenheit = Math.round((cel*9/5)+32);
+        return fahrenheit;
+    }
+
+    const fahToCel=(fahr) =>{
+        let celsius = Math.round((fahr-32)*5/9);
+        return celsius;
+    }
+    
+    let result;
+
+    if(valueTemp=='cel') {
+        result= celToFah(numberTemp);
+        document.getElementById('resultContainar').innerHTML= `= ${result}℉ahrenheit`;
+    }else{
+        result= fahToCel(numberTemp);
+        document.getElementById('resultContainar').innerHTML= `= ${result}℃elsius`;
+    }
+} 
